cc1795b54cfb7510d0d15af014967691306c7f53
Atanas Krstev <Atanas.Krstev@ipsos.com>
2024-03-25 10:07:36 +0200

    adding serach box

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,13 @@
-<script setup>
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" placeholder="Search for city or state"
+      v-model="searchQuery"
+      class="py-2 px-1 w-full bg-transparent border-b focus:border-wather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+    </div>
   </main>
 </template>
+
+<script setup>
+const searchQuery = ref("")
+</script>
